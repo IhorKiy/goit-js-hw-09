b66b33beece6a87d62e369e7b74efd6a62e518f4
IhorKiy <kiyanichenko@ukr.net>
2023-03-12 22:16:38 +0200

    3/12

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const ref = {
+  body: document.querySelector('body'),
+  btnStrt: document.querySelector('button[data-start]'),
+  btnStp: document.querySelector('button[data-stop]'),
+};
 
+ref.btnStrt.addEventListener('click', onClickStrt);
+ref.btnStp.addEventListener('click', onClickStp);
+
+function onClickStrt() {
+  interval = setInterval(() => {
+    ref.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  ref.btnStrt.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onClickStp() {
+  if (interval) {
+    clearInterval(interval);
+  }
+  ref.btnStrt.disabled = false;
+  ref.body.style.backgroundColor = '#FFFFFF';
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,53 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const ref = {
+  delayFrst: document.querySelector('[name="delay"]'),
+  delayStp: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+  btn: document.querySelector('BUTTON'),
+};
+
+
+ref.btn.addEventListener('click', createPromiseAll);
+
+async function createPromiseAll(e) {
+  e.preventDefault();
+  await createPosition();
+}
+
+async function createPosition() {
+  const amount = Number(ref.amount.value);
+  const arr1 = [];
+  for (let i = 1; i <= amount; i++) {
+    arr1.push(i);
   }
+
+  let accumulator = Number(ref.delayFrst.value);
+  const numElements = arr1.length;
+
+  const arr2 = [];
+  for (let i = 0; i < numElements; i++) {
+    arr2.push(accumulator);
+    accumulator += Number(ref.delayStp.value);
+  }
+
+  for (let i = 0; i < numElements; i++) {
+    try {
+      const result = await createPromise(arr1[i], arr2[i]);
+      console.log(`✅ Fulfilled promise ${result.position} in ${result.delay}ms`);
+    } catch (error) {
+      console.log(`❌ Rejected promise ${error.position} in ${error.delay}ms`);
+    }
+  }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
